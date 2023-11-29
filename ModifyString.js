d184f7b7c28217d76600c9832a4f89110239bf61
PraveenDassK <praveendass@yahoo.com>
2023-11-29 12:34:49 +0530

    modify and short string

---
+++ ModifyString.js
@@ -0,0 +1,30 @@
+//Q2 in Shikshalokam Assignment
+
+function modifyString(s) {
+  let result = "";
+  let previousChanged = false;
+  for (let i = 0; i < s.length; i++) {
+    let charCode = s.charCodeAt(i);
+    if (charCode % 2 === 0 && !previousChanged) {
+      let newCharCode = charCode + (charCode % 7);
+      result += String.fromCharCode(newCharCode);
+      console.log(result, "for even");
+
+      previousChanged = true;
+    } else if (charCode % 2 !== 0 && i > 0 && !previousChanged) {
+      let newCharCode = s.charCodeAt(i - 1) - (charCode % 5);
+      newCharCode = newCharCode < 0 ? 83 : newCharCode;
+      result = result.slice(0, -1) + String.fromCharCode(newCharCode) + s[i];
+      console.log(result, "for odd");
+
+      previousChanged = true;
+    } else {
+      result += s[i];
+      previousChanged = false;
+    }
+  }
+
+  return result;
+}
+let finalResult = modifyString("sHQen");
+console.log(finalResult, "this is final output");
--- shortSubstring.js
+++ shortSubstring.js
@@ -1,24 +1,26 @@
-function printShortestSubstrings(s, x) {
-  let shortestSubstrings = [];
+//Q1 in Shikshalokam Assignment
+
+function findingShortestSubstrings(s, x) {
+  let shortestSubstr = [];
   for (let i = 0; i < s.length; i++) {
     for (let j = i + x - 1; j < s.length; j++) {
       if (s[i] === s[j]) {
         let substring = s.substring(i, j + 1);
         if (
           substring.length >= x &&
-          (!shortestSubstrings.length ||
-            substring.length === shortestSubstrings[0].length)
+          (!shortestSubstr.length ||
+            substring.length === shortestSubstr[0].length)
         ) {
-          shortestSubstrings.push(substring);
-        } else if (substring.length < shortestSubstrings[0].length) {
-          shortestSubstrings = [substring];
+          shortestSubstr.push(substring);
+        } else if (substring.length < shortestSubstr[0].length) {
+          shortestSubstr = [substring];
         }
       }
     }
   }
 
-  if (shortestSubstrings.length) {
-    for (let substr of shortestSubstrings) {
+  if (shortestSubstr.length) {
+    for (let substr of shortestSubstr) {
       console.log(substr);
     }
   } else {
@@ -28,6 +30,6 @@ function printShortestSubstrings(s, x) {
 
 let s = "abccdbacca";
 
-let x = 5;
+let x = 4;
 console.log("x =", x);
-printShortestSubstrings(s, x);
+findingShortestSubstrings(s, x);
